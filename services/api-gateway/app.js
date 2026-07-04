983577b40d1c34cbf433986dbdce90570d9cddd2
Prashant Sachan <Prashant.sachan@isonxperiences.com>
2026-07-04 22:11:33 +0530

    feat: enhance CORS configuration and add UUID dependency
    - Updated CORS settings in the API gateway to allow requests from specific origins with credentials.
    - Added `uuid` package as a dependency in both package.json and package-lock.json for unique identifier generation.

--- services/api-gateway/app.js
+++ services/api-gateway/app.js
@@ -10,7 +10,12 @@ import morgan from "morgan";
 
 
 const app = express()
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    credentials: true,
+  }),
+);
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
